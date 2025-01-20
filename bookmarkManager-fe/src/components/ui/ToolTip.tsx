a0cc34ccd1e0fe3cef77809c9063d9c37a3fe3bc
aaronwrites <aaronh.official@gmail.com>
2025-01-20 14:40:41 +0530

    Added transition effect to the tooltip

--- bookmarkManager-fe/src/components/ui/ToolTip.tsx
+++ bookmarkManager-fe/src/components/ui/ToolTip.tsx
@@ -9,7 +9,7 @@ const ToolTip = ({ children, text } : ToolTipTypes) => {
   return (
     <div className="relative group">
         {children}
-        <div className="absolute bg-gray-800 text-white text-xs hidden group-hover:block left-1/2 top-full -translate-x-1/2 rounded-md px-2 py-1 mt-2 z-10">
+        <div className="absolute bg-gray-800 text-white text-xs left-1/2 top-full -translate-x-1/2 rounded-md px-2 py-1 mt-2 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
             {text}
         </div>
     </div>
