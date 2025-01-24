65f72410797e8297ad91a24619044296a3a05c0d
aaronwrites <aaronh.official@gmail.com>
2025-01-24 19:41:08 +0530

    Added support for tweets and made card component error proof by adding fallbacks

--- bookmarkManager-fe/src/components/ui/ContentCard.tsx
+++ bookmarkManager-fe/src/components/ui/ContentCard.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query"
 import { getMeta } from "../../services/previewService"
-import { Trash2, ExternalLink } from "lucide-react"
+import { Trash2, ExternalLink, Maximize2 } from "lucide-react"
 import fallbackImg from "../../assets/image.png"
 import { contentType } from "../../types/contentTypes"
+import { Tweet } from "react-tweet"
 
 type ContentCardProps = {
   content: contentType,
@@ -10,32 +11,42 @@ type ContentCardProps = {
 }
 
 const ContentCard = ({content, onClickHandler} : ContentCardProps) => {
-  const { data , isError, error } = useQuery({
+  const { data , isError } = useQuery({
     queryKey: ["preview", content.link],
-    queryFn: () => getMeta(content.link)
+    queryFn: () => getMeta(content.link),
+    retry: false
   })
+  
+  const cardTitle = content.title || data?.title || content.link;
+  const isTweet = content.link.includes("x.com") && content.link.includes("/status/");
 
-  if(isError) {
-    console.error(error);
-    return <div>Error loading Preview</div>
-  }
-
-  data ? console.log(data) : null
 
   return (
     <div className="flex flex-col h-auto justify-center items-center group gap-2 cursor-pointer m-1.5 w-full" onClick={onClickHandler}>
       <div className="relative grow self-stretch">
-        <div className="relative h-full rounded-xl flex flex-col shadow-xl overflow-hidden">
-          <img src={data?.image ? data.image : fallbackImg} alt="Preview" className="w-full object-center" />
+        <div className="relative h-full rounded-xl flex flex-col shadow-xl overflow-hidden" data-theme="light">
+          {isTweet ? (
+              <Tweet id={content.link.split("/status/")[1]} />
+            ) : (
+              <img
+                src={isError ? fallbackImg : data?.image || fallbackImg}
+                alt="Preview"
+                className="w-full object-center"
+              />
+            )}
           <div className="absolute inset-0 w-full h-full bg-black opacity-0 backdrop-blur-sm bg-opacity-0 group-hover:bg-opacity-50 group-hover:opacity-100 p-4 transition-opacity ease-out duration-300">
             <div className="w-full h-full flex flex-col justify-between">
               <div className="text-white">
                 <div className="flex justify-between items-center">
-                  <p className="text-white font-medium truncate">{data?.title}</p>
+                  <p className="text-white font-medium truncate">{cardTitle}</p>
                   <button className="rounded-full bg-red-500 hover:bg-red-600 p-1.5">
                     <Trash2 size={20} />
                   </button>
                 </div>
+              </div>
+              <div className="text-white/80 self-center tracking-wider flex items-center gap-2 font-bold">
+                Click to expand
+                <Maximize2 size={18} />
               </div>
               <div className="text-white flex justify-end w-full">
                   <div className="flex items-center gap-2 text-sm px-2 py-1 bg-black backdrop-blur-lg bg-opacity-30 rounded-full border border-gray-200 border-opacity-30 max-w-56">
@@ -50,7 +61,7 @@ const ContentCard = ({content, onClickHandler} : ContentCardProps) => {
         </div>
       </div>
       <div className="w-full text-sm text-muted px-3">
-        {data?.title}
+        {cardTitle}
       </div>
     </div>
   )
