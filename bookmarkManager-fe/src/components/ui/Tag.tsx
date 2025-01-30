46df3b724b5561ce1bb3a923eaa56c39770f1531
aaronwrites <aaronh.official@gmail.com>
2025-01-30 13:45:05 +0530

    Fixed the tag component's overflow behavior

--- bookmarkManager-fe/src/components/ui/Tag.tsx
+++ bookmarkManager-fe/src/components/ui/Tag.tsx
@@ -30,15 +30,15 @@ const Tag = ({ id, removeTag }: TagProps) => {
         return <span className="p-2 border border-primary text-primary rounded-full">Loading...</span>
     }
 
-	return <span className="relative transition-colors px-3 py-1.5 bg-muted/20 text-muted hover:bg-primary hover:text-white rounded-full group cursor-pointer max-w-32 truncate" onClick={handleClick}>
-        {data?.tagName}
+	return <div className="relative transition-colors px-3 py-1.5 bg-muted/20 text-muted hover:bg-primary hover:text-white rounded-full group cursor-pointer" onClick={handleClick}>
+        <div className="max-w-32 truncate">{data?.tagName}</div>
         { removeTag && <button
         onClick={handleDelete}
         className="absolute z-50 hidden group-hover:block -top-2 -right-2 text-xs text-primary bg-background border border-primary rounded-full px-1.5 py-0.5"
       >
         X
       </button>}
-    </span>;
+    </div>;
 };
 
 export default Tag;
